8cc1325e950db18bdda82d7ace138b50f26780e6
sarthaksd07 <sarthaksd07@gmail.com>
2021-06-08 19:27:29 +0530

    Added:Stored the day and the daily wage along with the total wage

--- empWage7.sh
+++ empWage7.sh
@@ -1,25 +1,31 @@
 #!/bin/bash -x
+
+#Constants
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
-NUM_OF_WORKING_DAYS=10
+NUM_OF_WORKING_DAYS=20
 MAX_HRS_IN_MONTH=50
+
 #Variable
 totalEmpHrs=0
 totalWorkingDays=0
+
+declare -A empDailyWage
+
 function getWorkingHours() {
-   case $1 in
+	case $1 in
       $IS_FULL_TIME)
-      empHrs=8
+         empHrs=8
       ;;
       $IS_PART_TIME)
-      empHrs=4
+         empHrs=4
       ;;
-		*)
-		empHrs=0
-		;;
-		esac
-		echo $empHrs
+      *)
+         empHrs=0
+      ;;
+   esac
+	echo $empHrs
 }
 
 function calculateWage() {
@@ -27,14 +33,14 @@ function calculateWage() {
 	wage=$(($EMP_RATE_PER_HR*$workHours))
 	echo $wage
 }
-
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_OF_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-   empCheck=$((RANDOM%2))
+	empCheck=$((RANDOM%2))
 	workHours="$( getWorkingHours $empCheck )"
 	totalEmpHrs=$(($totalEmpHrs+$workHours))
 	empDailyWage[$totalWorkingDays]="$( calculateWage $workHours )"
 done
-wage=$(($totalEmpHrs*$EMP_RATE_PER_HR))
-echo ${empDailyWage[@]}
+wage=$(($EMP_RATE_PER_HR*$totalEmpHrs))
+echo "Employee daily wage " ${empDailyWage[@]}
+echo "Day :: " ${!empDailyWage[@]}
